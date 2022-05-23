f9fbfa8df434a786dbad6eae1041b00e7c3c7e4e
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2022-05-22 22:17:18 -0500

    ended

---
+++ src/app/pages/no-page-found/no-page-found.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-no-page-found',
+  templateUrl: './no-page-found.component.html',
+  styleUrls: ['./no-page-found.component.scss']
+})
+export class NoPageFoundComponent implements OnInit {
+  public actualYear: number = new Date().getFullYear();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
